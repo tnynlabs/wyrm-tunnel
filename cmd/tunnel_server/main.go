6bf75815c334653eab6ecfd0d9737828aeb1a228
Zeyad Yasser <zeyady98@gmail.com>
2021-04-24 23:28:52 +0200

    create server run cmd
    Signed-off-by: Zeyad Yasser <zeyady98@gmail.com>

---
+++ cmd/tunnel_server/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"sync"
+	"log"
+	
+	"github.com/tnynlabs/wyrm-tunnel/pkg/tunnels"
+	"github.com/tnynlabs/wyrm-tunnel/pkg/manager"
+	"github.com/tnynlabs/wyrm-tunnel/pkg/transport/tungrpc"
+	"github.com/tnynlabs/wyrm/pkg/storage/postgres"
+	"github.com/tnynlabs/wyrm/pkg/devices"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Load environment variables from .env file
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file (error: %v)", err)
+	}
+
+	db, err := postgres.GetFromEnv()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	deviceRepo := postgres.CreateDeviceRepository(db)
+	deviceService := devices.CreateDeviceService(deviceRepo)
+
+	registry := tunnels.CreateRegistry()
+
+	// Wait group to allow server to notify main when they exit
+	// so we can cleanup before exiting
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+
+	managerServer := manager.NewServer(registry)
+	go func() {
+		log.Println("Running tunnel manager server...")
+		err := manager.RunServer(":5050", managerServer)
+		if err != nil {
+			log.Printf("Failed to run tunnel manager server (%v)\n", err)
+		}
+		wg.Done()
+	}()
+
+	grpcTunnelServer := tungrpc.NewServer(registry, deviceService)
+	go func() {
+		log.Println("Running grpc device tunnel transport server...")
+		err := tungrpc.RunServer(":5051", grpcTunnelServer)
+		if err != nil {
+			log.Printf("Failed to run grpc device tunnel transport server (%v)\n", err)
+		}
+		wg.Done()
+	}()
+
+	// If any of the running servers cleanup and exit
+	wg.Wait()
+	// TODO: Cleanup
+}
